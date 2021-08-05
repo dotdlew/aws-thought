61719ad2e94f0517f009a84f63a9b122c27e1ccc
Daniel Lewis <daniel.lewis@protonmail.com>
2021-08-05 13:28:51 -0600

    branding

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by dotdlew</div>
+      <div className="container">&copy;2021 by <a href="https://github.com/dotdlew">dotdlew</a></div>
     </footer>
   );
 };
