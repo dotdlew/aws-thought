d8f54b40ababff3292b5377f0db72bc0ad2fefeb
Daniel H. Lewis <daniel.lewis@protonmail.com>
2021-08-27 14:18:01 -0600

    Update index.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2021 by <a href="https://github.com/dotdlew">dotdlew</a></div>
+      <div className="container">&copy;2021 by <a href="https://github.com/dotdlew" target="_blank">dotdlew</a></div>
       <div className="container">* DISCLAIMER * This is a student project. It is not intended for official use. The views and opinions posted on this page are not a reflection of the app creator or host. All posts are made anonymously.</div>
     </footer>
   );
